8e12ceeb8c16b095121b04d3117b43a8a6811cef
Nicholas Smith <nsmith@ethosgroup.com>
2021-12-22 12:13:56 -0600

    Changed backend language and framework to Go and Gin

--- nhl-scoreboard-frontend/src/NhlScoreboard.ts
+++ nhl-scoreboard-frontend/src/NhlScoreboard.ts
@@ -1,7 +1,11 @@
 import { html, LitElement } from 'lit';
+import { HttpMixin } from './services/http';
+import { bootstrap } from './styles';
 
-export class NhlScoreboard extends LitElement {
+export class NhlScoreboard extends HttpMixin(LitElement) {
+    static styles = [bootstrap];
+
     render() {
-        return html` <div>Hello world!</div> `;
+        return html`<button class="btn btn-blue">Testing Bootstrap</button>`;
     }
 }
---
+++ nhl-scoreboard-frontend/src/styles.ts
@@ -0,0 +1,7 @@
+import { css } from 'lit';
+
+export const bootstrap = css`
+    @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+`;
+
+/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJzdHlsZXMudHMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjc3MgfSBmcm9tICdsaXQnO1xuXG5leHBvcnQgY29uc3QgYm9vdHN0cmFwID0gY3NzYFxuICAgIEBpbXBvcnQgJ2Jvb3RzdHJhcC9kaXN0L2Nzcy9ib290c3RyYXAuY3NzJztcbmA7XG4iXX0= */
